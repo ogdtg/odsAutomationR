bfc38f1c3297dbfe4483c49e066bd6d0786a9100
FLorenz <felixlorenz18@gmail.com>
2025-08-25 12:01:02 +0000

    add resources from url

--- R/add_file_to_dataset.R
+++ R/add_file_to_dataset.R
@@ -10,19 +10,44 @@
 #' @return resource_uid
 #' @export
 #'
-add_file_to_dataset <- function(filepath,dataset_uid,encoding){
+add_file_to_dataset <- function(filepath,dataset_uid,encoding,separator=NULL){
+
+  if (grepl("^http", filepath)) {
+
+    body <- list(
+      type = "csvfile",
+      title = filepath,
+      params = list(
+        doublequote = T,
+        encoding = encoding,
+        separator = separator
+      ),
+      datasource = list(type = "http",
+                        connection = list(type = "http",
+                                          url = sub("^(https?://[^/]+).*", "\\1", filepath),
+                                          auth = list(type = "basic_auth",
+                                                      username = "test",
+                                                      password = "test")),
+                        relative_url = sub("^https?://[^/]+", "", filepath))
+    ) %>%
+      jsonlite::toJSON(auto_unbox = T)
+
+  } else {
+
+    result <- upload_file_to_ods(dataset_uid=dataset_uid,filepath = filepath)
+
+    body <- list(type = "csvfile",
+                 title = result$filename,
+                 params = list(doublequote = T,
+                               encoding = encoding,
+                               separator = ","),
+                 datasource = list(type = "uploaded_file",
+                                   file = list(uid = result$uid))) %>%
+      jsonlite::toJSON(auto_unbox = T)
+
+  }
 
 
-  result <- upload_file_to_ods(dataset_uid=dataset_uid,filepath = filepath)
-
-  body <- list(type = "csvfile",
-               title = result$filename,
-               params = list(doublequote = T,
-                             encoding = encoding,
-                             separator = ","),
-               datasource = list(type = "uploaded_file",
-                                 file = list(uid = result$uid))) %>%
-    jsonlite::toJSON(auto_unbox = T)
 
 
   res <- httr::POST(url = paste0('https://',getDomain(),'/api/',getApiType(),'/datasets/',dataset_uid,"/resources/"), body = body,
--- R/update_resource.R
+++ R/update_resource.R
@@ -9,12 +9,16 @@
 #'
 #' @export
 #'
-update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding){
+update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding,separator = NULL){
+
+
+
 
   if (is.null(resource_uid)) {
     resource_info <- get_dataset_resource(dataset_uid = dataset_uid)
     if (length(resource_info$results)==0) {
       message("There is no resource to update. Will add new resource")
+
       resource_uid <- add_file_to_dataset(filepath = filepath, dataset_uid = dataset_uid, encoding = encoding)
       return(resource_uid)
     }
@@ -24,9 +28,19 @@ update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding
     }
 
     resource_uid <- resource_info$results$uid[nrow(resource_info$results)]
+
+    if (grepl("^http", filepath)) {
+      delete_resource(dataset_uid = dataset_uid,resource_uid = resource_uid)
+      resource_id <- add_file_to_dataset(filepath=filepath,dataset_uid=dataset_uid,encoding=encoding,separator=separator)
+      return(resource_id)
+    }
+
+
   }
 
-  results <- upload_file_to_ods(filepath = filepath,dataset_uid = dataset_uid)
+
+
+  results <- upload_file_to_ods(filepath = filepath, dataset_uid = dataset_uid)
 
 
   body <- list(
@@ -37,10 +51,12 @@ update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding
       encoding = encoding,
       separator = ","
     ),
-    datasource = list(type = "uploaded_file",
-                      file = list(uid = results$uid))
+    datasource = list(type = "uploaded_file", file = list(uid = results$uid))
   ) %>%
-    jsonlite::toJSON(auto_unbox = T)
+    jsonlite::toJSON(auto_unbox = T, null = "null")
+
+
+
 
   res <- httr::PUT(url = paste0("https://",getDomain(),"/api/",getApiType(),"/datasets/",dataset_uid,"/resources/",resource_uid,"/"),
              body = body,
