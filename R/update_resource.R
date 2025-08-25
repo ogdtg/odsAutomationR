0cf7ef3f589fd1e2a970ed6bf905f6f664a4e6a9
FLorenz <felixlorenz18@gmail.com>
2025-08-25 12:19:00 +0000

    add resources from url and make t possible to add multiple resources

--- R/update_resource.R
+++ R/update_resource.R
@@ -6,11 +6,11 @@
 #' @template encoding
 #' @template filepath
 #' @template resource_uid
-#' @param separator
+#' @param separator separator of csv from http
 #'
 #' @export
 #'
-update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding,separator = NULL){
+update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding,separator = NULL,replace = TRUE){
 
 
 
@@ -31,7 +31,11 @@ update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding
     resource_uid <- resource_info$results$uid[nrow(resource_info$results)]
 
     if (grepl("^http", filepath)) {
-      delete_resource(dataset_uid = dataset_uid,resource_uid = resource_uid)
+
+      if (replace==TRUE){
+        delete_resource(dataset_uid = dataset_uid,resource_uid = resource_uid)
+      }
+
       resource_id <- add_file_to_dataset(filepath=filepath,dataset_uid=dataset_uid,encoding=encoding,separator=separator)
       return(resource_id)
     }
@@ -40,35 +44,40 @@ update_resource <-  function(dataset_uid, resource_uid = NULL,filepath, encoding
   }
 
 
-
-  results <- upload_file_to_ods(filepath = filepath, dataset_uid = dataset_uid)
-
-
-  body <- list(
-    type = "csvfile",
-    title = results$filename,
-    params = list(
-      doublequote = T,
-      encoding = encoding,
-      separator = ","
-    ),
-    datasource = list(type = "uploaded_file", file = list(uid = results$uid))
-  ) %>%
-    jsonlite::toJSON(auto_unbox = T, null = "null")
-
-
-
-
-  res <- httr::PUT(url = paste0("https://",getDomain(),"/api/",getApiType(),"/datasets/",dataset_uid,"/resources/",resource_uid,"/"),
-             body = body,
-             query = list(apikey = getKey()),
-             httr::accept_json(),
-             httr::content_type_json())
-
-  if (res$status_code %in% c(200,201,204)){
+  if (replace==FALSE){
+    resource_uid <- add_file_to_dataset(filepath = filepath, dataset_uid = dataset_uid, encoding = encoding,separator = separator)
     return(resource_uid)
-  } else{
-    return(NULL)
+  } else {
+    results <- upload_file_to_ods(filepath = filepath, dataset_uid = dataset_uid)
+
+
+    body <- list(
+      type = "csvfile",
+      title = results$filename,
+      params = list(
+        doublequote = T,
+        encoding = encoding,
+        separator = ","
+      ),
+      datasource = list(type = "uploaded_file", file = list(uid = results$uid))
+    ) %>%
+      jsonlite::toJSON(auto_unbox = T, null = "null")
+
+
+
+
+    res <- httr::PUT(url = paste0("https://",getDomain(),"/api/",getApiType(),"/datasets/",dataset_uid,"/resources/",resource_uid,"/"),
+                     body = body,
+                     query = list(apikey = getKey()),
+                     httr::accept_json(),
+                     httr::content_type_json())
+
+    if (res$status_code %in% c(200,201,204)){
+      return(resource_uid)
+    } else{
+      return(NULL)
+    }
+
   }
 
 
